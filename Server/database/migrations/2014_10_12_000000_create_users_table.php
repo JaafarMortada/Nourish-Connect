d6244b03b65cfc4956e56f989121ff26f13f8a24
Jaafar Mortada <jaafar1899@outlook.com>
2023-09-08 01:28:43 +0300

    Server <> Adding User's Migrations & JWT

--- Server/database/migrations/2014_10_12_000000_create_users_table.php
+++ Server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -8,11 +8,40 @@ return new class extends Migration
 {
     public function up(): void
     {
+        Schema::create('usertypes', function (Blueprint $table) {
+            $table->id();
+            $table->string('usertype');
+            $table->longText('extra')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('locations', function (Blueprint $table) {
+            $table->id();
+            $table->float('latitude');
+            $table->float('longitude');
+            $table->timestamps();
+        });
+
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username');
+            $table->string('company_name');
+            $table->string('email')->unique();
+            $table->longText('password');
+            $table->unsignedBigInteger('usertype_id');
+            $table->text('pic_url')->nullable();
+            $table->unsignedBigInteger('location_id');
+            $table->timestamps();
+            $table->foreign('usertype_id')->references('id')->on('usertypes')->onDelete('cascade');
+            $table->foreign('location_id')->references('id')->on('locations')->onDelete('cascade');
+        });
 
     }
 
     public function down(): void
     {
-
+        Schema::dropIfExists('usertypes');
+        Schema::dropIfExists('locations');
+        Schema::dropIfExists('users');
     }
 };
