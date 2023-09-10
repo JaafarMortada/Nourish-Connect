a65890bda0a9b21b25b848d0a3529cccef0cf6aa
Jaafar Mortada <jaafar1899@outlook.com>
2023-09-10 20:21:58 +0300

    Client <> Adding GetItems Request

--- Client/src/components/cards/itemsList/ItemsList.jsx
+++ Client/src/components/cards/itemsList/ItemsList.jsx
@@ -1,49 +1,72 @@
 import {
-    MagnifyingGlassIcon,
-  } from "@heroicons/react/24/outline";
-  import {
-    Card,
-    CardHeader,
-    Input,
-    Typography,
-    CardBody,
-  } from "@material-tailwind/react";
+  MagnifyingGlassIcon,
+} from "@heroicons/react/24/outline";
+import {
+  Card,
+  CardHeader,
+  Input,
+  Typography,
+  CardBody,
+} from "@material-tailwind/react";
 import ItemCard from "../miniCards/ItemCard";
-
+import { sendRequest } from "../../../config/request";
+import { useEffect, useState } from "react";
+import { useStoreData } from "../../../global/store";
 const ItemsList = () => {
-       
+
+  const { store, setStoreData } = useStoreData()
+
+  const [itemsData, setItemsData] = useState([])
+
+  useEffect(() => {
+    const getItemsHandler = async () => {
+      try {
+        const response = await sendRequest({
+          method: "GET",
+          route: "/api/cashier/items/get_items",
+          token: store.token,
+        });
+        if (response.item) {
+          setItemsData(response.item);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getItemsHandler()
+  }, [])
+
   return (
     <Card className="h-[60%] w-full">
       <CardHeader floated={false} shadow={false} className="rounded-none pb-[10px]">
         <div className="mb-8 flex items-center justify-between gap-8  translate-y-[5px]">
           <div>
             <Typography variant="h5" color="blue-gray">
-                Items
+              Items
             </Typography>
           </div>
           <div className="flex shrink-0 flex-col gap-2 sm:flex-row w-[35%]">
             <Input
-                className="translate-y-[5px] "
-                labelProps={{
-                    className: "translate-y-[5px] "
-                }}
-                
-                label="Search Items"
-                icon={<MagnifyingGlassIcon className="h-5 w-5 translate-y-[5px]" />}
-                />
+              className="translate-y-[5px] "
+              labelProps={{
+                className: "translate-y-[5px] "
+              }}
+
+              label="Search Items"
+              icon={<MagnifyingGlassIcon className="h-5 w-5 translate-y-[5px]" />}
+            />
           </div>
         </div>
 
       </CardHeader>
       <CardBody className="overflow-scroll flex flex-wrap gap-x-10 gap-y-5 justify-around">
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
+        {
+          itemsData.map((item) => (
+            <ItemCard key={item.id} data={item} />
+          ))
+        }
       </CardBody>
-      
+
     </Card>
   );
 }
--- Client/src/components/cards/miniCards/ItemCard.jsx
+++ Client/src/components/cards/miniCards/ItemCard.jsx
@@ -1,20 +1,23 @@
 import { Tooltip } from "@material-tailwind/react";
 import { notFound } from "../../../assets";
 import { PlusIcon } from "@heroicons/react/24/solid";
-const ItemCard = () => {
+const ItemCard = ({ data }) => {
     return (
         <div className="flex items-center rounded-lg h-[60px] w-[220px] bg-[#E0F8EA] text-[--text-black]">
-            <img src={notFound} className="min-w-10 h-10 mx-2 rounded-md bg-[--accent]" />
+            <img
+                src={data.pic_url ? `http://127.0.0.1:8000/storage/${data.pic_url}` : notFound}
+                className="min-w-10 h-10 mx-2 rounded-md bg-[--accent]"
+            />
             <div className="flex flex-col ">
-                <Tooltip content={'item name'} className="bg-[--primary]">
-                    <span className="text-[--text-black] text-[21px] truncate w-[110px]">item name</span>
+                <Tooltip content={data.name} className="bg-[--primary]">
+                    <span className="text-[--text-black] text-[21px] truncate w-[110px]">{data.name}</span>
                 </Tooltip>
-                <span className="text-gray-700 text-[16px]">price</span>
+                <span className="text-gray-700 text-[16px]">{data.price} $</span>
             </div>
-            <div className="flex flex-1 justify-center"> 
-            <Tooltip className="bg-[--primary]" content={'Add to Cart'}>
-                <PlusIcon className="w-[30px] h-[30px] text-[--primary] hover:scale-[1.2] transition-all cursor-pointer"/>
-            </Tooltip>
+            <div className="flex flex-1 justify-center">
+                <Tooltip className="bg-[--primary]" content={'Add to Cart'}>
+                    <PlusIcon className="w-[30px] h-[30px] text-[--primary] hover:scale-[1.2] transition-all cursor-pointer" />
+                </Tooltip>
             </div>
         </div>
     )
