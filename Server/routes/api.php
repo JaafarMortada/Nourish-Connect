2c6157c45199332cf2aa306e63613b04e2d8f015
Jaafar Mortada <jaafar1899@outlook.com>
2023-09-11 13:45:09 +0300

    Server <> Adding Manager Middleware

--- Server/routes/api.php
+++ Server/routes/api.php
@@ -5,6 +5,13 @@ use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\ItemsController;
 use Illuminate\Support\Facades\Route;
 
+
+Route::group(["middleware" => "manager", "prefix" => "/manager"], function () {
+    Route::post('add_cashier', [AuthController::class, 'addCashier']);
+
+});
+
+
 Route::group(["middleware" => "cashier", "prefix" => "/cashier"], function () {
     Route::prefix('/items')->group(function () {
         Route::get("/get_items/{search?}", [ItemsController::class, "getItems"]);
@@ -19,6 +26,5 @@ Route::prefix('auth')->group(function () {
     Route::post('login', [AuthController::class, 'login']);
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('register', [AuthController::class, 'register']);
-    Route::post('add_cashier', [AuthController::class, 'addCashier']);
     Route::post('refresh', [AuthController::class, 'refresh']);
 });
