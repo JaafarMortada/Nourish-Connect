ae0cf7a43406ba4c91b57617ffb92b11940bc92b
Jaafar Mortada <jaafar1899@outlook.com>
2023-09-12 01:06:16 +0300

    Client <> Adding UploadLogo Request

--- Client/src/components/cards/profileCard/ProfileCard.jsx
+++ Client/src/components/cards/profileCard/ProfileCard.jsx
@@ -23,9 +23,10 @@ const ProfileCard = () => {
     const [profileData, setProfileData] = useState({})
     const [loading, setLoading] = useState(true)
     const [isUnauthorized, setIsUnauthorized] = useState()
+    const [imageFile, setImageFile] = useState(null)
     const handleLogoChange = (e) => {
         const file = e.target.files[0];
-
+        setImageFile(file)
         if (file) {
             const reader = new FileReader();
             reader.onload = (e) => {
@@ -62,89 +63,110 @@ const ProfileCard = () => {
     }, [])
 
     const handleLogoUpload = async () => {
-
+        try {
+            const formData = new FormData();
+            formData.append("image", imageFile);
+            const response = await sendRequest({
+                method: "POST",
+                route: "/api/edit_profile",
+                token: store.token,
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+                body: formData,
+            });
+            if (response.message === "success") {
+                setExistingLogo(newLogo)
+                setNewLogo(null)
+            } else if (response.message === "Unauthorized") {
+                setIsUnauthorized(true)
+            }
+        } catch (error) {
+            console.log(error);
+            
+        }
     }
 
     return (
         isUnauthorized ? <Unauthorized /> :
-        <>
+            <>
 
-            <Card
-                className={`flex flex-col md:min-h-[50%] min-h-[80vh] md:w-[95%] w-[400px] ${loading ? "justify-center items-center" : ""}`}
-            >
+                <Card
+                    className={`flex flex-col md:min-h-[50%] min-h-[80vh] md:w-[95%] w-[400px] ${loading ? "justify-center items-center" : ""}`}
+                >
 
-                {loading ? <Spinner className="w-20 h-20" /> :
-                    <CardBody className="overflow-scroll px-5 flex-1 flex md:flex-row flex-col justify-between items-center md:gap-0 gap-10">
-                        <div className="w-[300px] h-fit flex flex-col items-center justify-center gap-10">
-                            <Avatar
-                                src={newLogo ? newLogo :
-                                    existingLogo ? existingLogo :
-                                        default_profile_pic
-                                }
-                                withBorder={true}
-                                className="w-[200px] h-[200px] border-[--primary]"
-                            />
-                            {
-                                newLogo ?
-                                    <div className="flex gap-5">
-                                        <PrimaryButton
-                                            label={"Cancel"}
-                                            classNames={"bg-[--primary] w-[100px]"}
-                                            onClick={() => setNewLogo(null)}
+                    {loading ? <Spinner className="w-20 h-20" /> :
+                        <CardBody className="overflow-scroll px-5 flex-1 flex md:flex-row flex-col justify-between items-center md:gap-0 gap-10">
+                            <div className="w-[300px] h-fit flex flex-col items-center justify-center gap-10">
+                                <Avatar
+                                    src={newLogo ? newLogo :
+                                        existingLogo ? existingLogo :
+                                            default_profile_pic
+                                    }
+                                    withBorder={true}
+                                    className="w-[200px] h-[200px] border-[--primary]"
+                                />
+                                {
+                                    newLogo ?
+                                        <div className="flex gap-5">
+                                            <PrimaryButton
+                                                label={"Cancel"}
+                                                classNames={"bg-[--primary] w-[100px]"}
+                                                onClick={() => setNewLogo(null)}
+                                            />
+                                            <PrimaryButton
+                                                label={"Confirm"}
+                                                classNames={"bg-[--primary] w-[100px]"}
+                                                onClick={handleLogoUpload}
+                                            />
+                                        </div>
+
+                                        :
+                                        <InputField
+                                            type={"file"}
+                                            label={"Upload or Edit your logo"}
+                                            onChange={handleLogoChange}
+                                            error={false}
                                         />
-                                        <PrimaryButton
-                                            label={"Confirm"}
-                                            classNames={"bg-[--primary] w-[100px]"}
-                                            onClick={handleLogoUpload}
+                                }
+
+                            </div>
+                            <div className="w-[65%] flex flex-col h-full gap-5">
+                                <div className="w-full">
+                                    <Typography variant="h3" color="black">
+                                        {profileData.username}
+                                    </Typography>
+                                    <Typography color="gray" className="font-normal ">
+                                        Manager at a {profileData.company_name}
+                                    </Typography>
+                                </div>
+                                <div className="flex h-full lg:flex-row flex-col ">
+                                    <div className="flex-wrap flex lg:flex-row flex-col gap-10 items-center">
+                                        <UserInfoCard
+                                            icon={<EnvelopeIcon />}
+                                            title={"Email"}
+                                            info={[profileData.email]}
+                                        />
+
+                                        <UserInfoCard
+                                            icon={<MapPinIcon />}
+                                            title={"Location"}
+                                            info={[`Latitude: ${profileData.latitude}`, `longitude: ${profileData.longitude}`]}
+                                        />
+                                        <UserInfoCard
+                                            icon={<BiSolidDonateHeart className="w-8 h-8" />}
+                                            title={"Donations"}
+                                            info={[`Delivered: ${profileData.donations_count}`]}
                                         />
                                     </div>
-
-                                    :
-                                    <InputField
-                                        type={"file"}
-                                        label={"Upload or Edit your logo"}
-                                        onChange={handleLogoChange}
-                                        error={false}
-                                    />
-                            }
-
-                        </div>
-                        <div className="w-[65%] flex flex-col h-full gap-5">
-                            <div className="w-full">
-                                <Typography variant="h3" color="black">
-                                    {profileData.username}
-                                </Typography>
-                                <Typography color="gray" className="font-normal ">
-                                    Manager at a {profileData.company_name}
-                                </Typography>
-                            </div>
-                            <div className="flex h-full lg:flex-row flex-col ">
-                                <div className="flex-wrap flex lg:flex-row flex-col gap-10 items-center">
-                                    <UserInfoCard
-                                        icon={<EnvelopeIcon />}
-                                        title={"Email"}
-                                        info={[profileData.email]}
-                                    />
-
-                                    <UserInfoCard
-                                        icon={<MapPinIcon />}
-                                        title={"Location"}
-                                        info={[`Latitude: ${profileData.latitude}`, `longitude: ${profileData.longitude}`]}
-                                    />
-                                    <UserInfoCard
-                                        icon={<BiSolidDonateHeart className="w-8 h-8" />}
-                                        title={"Donations"}
-                                        info={[`Delivered: ${profileData.donations_count}`]}
-                                    />
                                 </div>
                             </div>
-                        </div>
-                    </CardBody>}
+                        </CardBody>}
 
-            </Card>
+                </Card>
 
 
-        </>
+            </>
     )
 }
 
