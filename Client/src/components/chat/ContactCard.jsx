fb4cdd6cf2ca669215afaced6434bd7576f10bf9
Jaafar Mortada <jaafar1899@outlook.com>
2023-09-13 16:28:52 +0300

    Client <> Adding Search Contacts Request

--- Client/src/components/chat/ContactCard.jsx
+++ Client/src/components/chat/ContactCard.jsx
@@ -1,16 +1,16 @@
 import { Avatar } from "@material-tailwind/react"
 import { default_profile_pic } from "../../assets"
 
-const ContactCard = ({ headerCard = false }) => {
+const ContactCard = ({ headerCard = false, data }) => {
     return (
         <div className={`flex items-center gap-4 pl-2 rounded-t-md flex-1 ${headerCard ? "" : "hover:bg-[#2c2c2c] cursor-pointer"} `}>
-            <Avatar src={default_profile_pic} alt="avatar" />
-            <div className={`flex flex-1 flex-col justify-center ${headerCard ? "" : "border-b-[1px] border-gray-700 pb-2"}`}>
-                <span className={`text-[--text-gray] ${headerCard ? "text-[27px] max-w-[400px]" : "text-[21px] max-w-[200px]"}]  truncate font-bold `}>username</span>
-                {
+            <Avatar src={data && data.pic_url ? `http://127.0.0.1:8000/storage/${data.pic_url}` : default_profile_pic} alt="avatar" />
+            <div className={`flex flex-1 py-4 flex-col justify-center ${headerCard ? "" : "border-b-[1px] border-gray-700 "}`}>
+                <span className={`text-[--text-gray] ${headerCard ? "text-[27px] max-w-[400px]" : "text-[21px] max-w-[200px]"}]  truncate font-bold `}>{data && data.company_name}</span>
+                {/* {
                     headerCard ? null :
                         <span className="text-[--text-gray] max-w-[170px] truncate">last message</span>
-                }
+                } */}
             </div>
         </div>
     )
--- Client/src/components/chat/ContactsContainer.jsx
+++ Client/src/components/chat/ContactsContainer.jsx
@@ -1,39 +1,81 @@
 import { Input } from "@material-tailwind/react"
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline"
 import ContactCard from "./ContactCard"
-
+import { useStoreData } from "../../global/store"
+import { useEffect, useState } from "react"
+import { sendRequest } from "../../config/request"
 const ContactsContainer = () => {
-  return (
-    <div className='min-w-[20rem] h-full bg-[--background-black] mb-2 p-4 border-l-4 border-black flex flex-col gap-4'>
-        <div className="flex shrink-0 flex-col gap-2 sm:flex-row">
-            <Input
-              placeholder="Search charities"
-              className="!border-0 !bg-gray-600 text-white placeholder:text-gray-100 !rounded-full "
-              labelProps={{
-                className: "hidden",
-              }}
-              containerProps={{ className: "min-w-[100px] " }}
-              
-            //   value={searchTerm}
-            //   onChange={(e) => setSearchTerm(e.target.value)}
+    const { store } = useStoreData()
 
-              icon={<MagnifyingGlassIcon className="h-5 w-5 text-white " />}
-            />
-          </div>
-          <hr className="my-2 border-[--text-gray]" />
-          <div className="flex flex-col gap-2 max-h-full overflow-scroll justify-start">
-            <ContactCard />
-            <ContactCard />
-            <ContactCard />
-            <ContactCard />
-            <ContactCard />
-            <ContactCard />
+    const [contacts, setContacts] = useState([])
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const useDebounce = (value, delay) => {
+        const [debouncedValue, setDebouncedValue] = useState(value);
+
+        useEffect(() => {
+            const timer = setTimeout(() => {
+                setDebouncedValue(value);
+            }, [delay]);
+
+            return () => {
+                clearTimeout(timer);
+            };
+        }, [value, delay]);
+
+        return debouncedValue;
+    };
+
+    const debouncedSearchTerm = useDebounce(searchTerm, 500);
+
+    useEffect(() => {
+        const getContactsHandler = async () => {
+            try {
+                const response = await sendRequest({
+                    method: "GET",
+                    route: `/api/search_users/${debouncedSearchTerm}`,
+                    token: store.token,
+                });
+                if (response.message === "success") {
+                    setContacts(response.contacts);
+                }
+            } catch (error) {
+                // console.log(error);
+            }
+        }
+        getContactsHandler()
+    }, [debouncedSearchTerm])
 
 
+    return (
+        <div className='min-w-[20rem] h-full bg-[--background-black] mb-2 p-4 border-l-4 border-black flex flex-col gap-4'>
+            <div className="flex shrink-0 flex-col gap-2 sm:flex-row">
+                <Input
+                    placeholder={`Search ${store.usertype === "manager" ? "charities" : "supermarkets"}`}
+                    className="!border-0 !bg-gray-600 text-white placeholder:text-gray-100 !rounded-full "
+                    labelProps={{
+                        className: "hidden",
+                    }}
+                    containerProps={{ className: "min-w-[100px] " }}
 
-          </div>
-    </div>
-  )
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+
+                    icon={<MagnifyingGlassIcon className="h-5 w-5 text-white " />}
+                />
+            </div>
+            <hr className="my-2 border-[--text-gray]" />
+            <div className="flex flex-col gap-2 max-h-full overflow-scroll justify-start">
+                {contacts.length > 0 ?
+                    contacts.map((contact) => (
+                        <ContactCard data={contact} key={contact.id} />
+
+                    )) :
+                    <span className="text-[--text-gray] text-center">You have no chats, search users and start a new chat </span>}
+
+            </div>
+        </div>
+    )
 }
 
 export default ContactsContainer
