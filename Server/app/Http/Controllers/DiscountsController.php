9f7794607a1859d7009deba479a9e5f538153d96
Jaafar Mortada <jaafar1899@outlook.com>
2023-09-18 18:46:06 +0300

    Server <> Adding Approve Discount API

--- Server/app/Http/Controllers/DiscountsController.php
+++ Server/app/Http/Controllers/DiscountsController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Discount;
+use App\Models\DiscountItem;
+use App\Models\DiscountSuggestion;
+use App\Models\Item;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Carbon\Carbon;
@@ -36,6 +40,47 @@ class DiscountsController extends Controller
         return response()->json([
             'message' => 'success',
             'discounts' => $responseDiscounts
+        ], 200);
+    }
+
+    public function approveDiscount(Request $request ,$suggestion_id) {
+        $discountSuggestion = DiscountSuggestion::where('id', $suggestion_id)->first();
+        $discountUntil = Carbon::parse($discountSuggestion->until);
+        if ($discountSuggestion->approved == 1 || !$discountUntil->isFuture()) {
+            return response()->json([
+                'message' => 'Error approving discount',
+            ]);
+        }
+
+        $discountSuggestion->approved = 1;
+
+        $item_id = $discountSuggestion->item_id;
+        $dbItem = Item::where('id', $item_id)->first();
+        if (is_null($dbItem) || $dbItem->available_quantity <= 0) {
+            return response()->json([
+                'message' => 'This item is sold out, discount approval failed',
+            ]);
+        }
+
+        $newDiscount = new Discount;
+        $newDiscount->title = $discountSuggestion->percentage * 100 . "% discount on " . $dbItem->name;
+        $newDiscount->description = $request->description;
+        $newDiscount->percentage = $discountSuggestion->percentage;
+        $newDiscount->company_id = Auth::id();
+        $newDiscount->until =  $discountSuggestion->until;
+        $newDiscount->save();
+
+        $newDiscountItem = new DiscountItem;
+        $newDiscountItem->item_id = $discountSuggestion->item_id;
+        $newDiscountItem->quantity = $dbItem->available_quantity;
+        $newDiscountItem->discount_id = $newDiscount->id;
+        $newDiscountItem->save();
+
+        $discountSuggestion->save();
+
+        return response()->json([
+            'message' => 'success',
+
         ], 200);
     }
 }
