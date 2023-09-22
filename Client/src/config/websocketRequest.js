d87886e3fc318b694a005e65ac9e3a680d0bef0d
Jaafar Mortada <jaafar1899@outlook.com>
2023-09-22 20:53:42 +0300

    Client <> Modifying WS Request

--- Client/src/config/websocketRequest.js
+++ Client/src/config/websocketRequest.js
@@ -2,14 +2,16 @@ import axios from "axios";
 
 export const websocketRequest = async ({
   inventoryId,
+  WSevent,
+  receiver_id,
 
 }) => {
-  if (!inventoryId) throw Error("inventoryId required");
-
+  if (!(inventoryId || receiver_id) && !WSevent) throw Error("URL Error");
+  const query = WSevent === 'items' ? `trigger-items-event?inventory_id=${inventoryId}` : WSevent === "donation" ? `trigger-donation-event?receiver_id=${receiver_id}` : ""
   try {
     const response = await axios.request({
       method: "GET",
-      url: `http://localhost:6001/ws/trigger-event?inventory_id=${inventoryId}`,
+      url: `http://localhost:6001/ws/${query}`,
     });
 
     return response.data;
