1336de8b864fa9d7e271542cf9c4482eb63df003
Jaafar Mortada <jaafar1899@outlook.com>
2023-09-24 15:49:40 +0300

    Client <> Adding Download Templates Component

---
+++ Client/src/components/ui/DownloadTemplate.jsx
@@ -0,0 +1,20 @@
+import PrimaryButton from './Button'
+
+const DownloadTemplate = ({ path, fileName }) => {
+    return (
+        <a
+            className="teacher-option-btn grade-card-btn"
+            href={`http://localhost:8000/storage/${path}/${fileName}`}
+            download={`${fileName}`}
+            target="_blank"
+        >
+            <PrimaryButton
+                label={"Download Template"}
+                classNames={`max-h-[40px] flex justify-center items-center w-[200px] bg-[--primary]`}
+
+            />
+        </a>
+    )
+}
+
+export default DownloadTemplate
